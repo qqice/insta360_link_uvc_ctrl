5f6147c2b05d0e05e09c5f9196cea8b95c816988
qqice <qqice@srhaze.com>
2024-01-22 02:35:02 +0800

    添加控制帧发送测试

--- main.cpp
+++ main.cpp
@@ -217,7 +217,7 @@ int main(int argc, char **argv) {
           // } else {
           //   uvc_perror(res, " ... uvc_set_ae_mode failed to enable auto exposure mode");
           // }
-          set_camera_control(devh);
+          set_camera_control(devh);//控制帧发送
           sleep(10); /* stream for 10 seconds */
 
           /* End the stream. Blocks until last callback is serviced */
