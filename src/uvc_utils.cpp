dc5873f462c39123a8c98574e1d64431e530e03f
qqice <39629131+qqice@users.noreply.github.com>
2024-10-27 01:48:11 +0800

    delete bitrate parameter to deliver better stream to server and then process(compress) it with mediamtx for smaller record size

--- src/uvc_utils.cpp
+++ src/uvc_utils.cpp
@@ -3,15 +3,14 @@
 int width = 1920;
 int height = 1080;
 int fps = 30;
-const int bitrate = 3000000;
-const char *rtsp_server = "rtsp://127.0.0.1:8554/mystream";
+const int bitrate = 0;
+const char *rtsp_server = "rtsp://127.0.0.1:8554/mainstream";
 const char *audio_device = "hw:5,0";
 const char *audio_encoder = "opusenc";
 
 uvc_device_handle_t *devh; // 定义全局变量devh
 
-cv::VideoWriter out("appsrc ! videoconvert ! mpph264enc level=40 bps=" + std::to_string(bitrate) +
-                    " ! queue ! s.sink_0 alsasrc device=" + audio_device + " ! queue ! audioconvert ! " + audio_encoder + " ! s.sink_1 rtspclientsink name=s location=" +
+cv::VideoWriter out(std::string("appsrc ! videoconvert ! mpph265enc ! queue ! s.sink_0 alsasrc device=") + audio_device + " ! queue ! audioconvert ! " + audio_encoder + " ! s.sink_1 rtspclientsink name=s location=" +
                     rtsp_server,
                     cv::CAP_GSTREAMER, 0, fps, cv::Size(width, height), true);
 
